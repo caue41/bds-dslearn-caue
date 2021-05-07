0b62bcd55eff65d30e78df0c33b417ba64c19ba7
Cauê Gonçalves Santana <caue41@gmail.com>
2021-05-07 19:01:26 -0300

    offer

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -13,4 +13,7 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
-INSERT INTO tb_course(name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://cdn.pixabay.com/photo/2020/05/31/09/30/online-course-5241968_1280.jpg', 'https://www.auralize.com/images/AM-PRO-RT-wide.jpg');
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://cdn.pixabay.com/photo/2020/05/31/09/30/online-course-5241968_1280.jpg', 'https://www.auralize.com/images/AM-PRO-RT-wide.jpg');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0',TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00.12345Z', TIMESTAMP WITH TIME ZONE '2020-11-21T03:00:00.12345Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0',TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00.12345Z', TIMESTAMP WITH TIME ZONE '2020-12-21T03:00:00.12345Z', 1);
