922b7e848d490339132b274c47d2a7ac45336a5b
Cauê Gonçalves Santana <caue41@gmail.com>
2021-05-15 17:37:45 -0300

    Validation, Exception handling

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dslearnbds.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+	static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
